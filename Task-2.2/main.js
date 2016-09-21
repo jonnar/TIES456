f35191e8f66dd2bd3f68b8dbd28a4032fc364b95
Matti Kinnunen <matti.kinnunen@jyu.fi>
2016-09-21 16:35:50 +0300

    Authorization and account info retrieval working.

--- Task-2.2/main.js
+++ Task-2.2/main.js
@@ -1,5 +1,21 @@
 var app_key = 'ojtyzbpz7peoc2v';
 var app_secret = 'm8kdx1fyrghmj5d';
+var access_token = null;
+
+function info() {
+  
+  var req = $.ajax({
+    url: 'https://api.dropboxapi.com/2/users/get_current_account',
+    type: 'POST',
+    success: function(data, status) {
+        console.log(data, status);
+        $('#data').text(JSON.stringify(data));
+    },
+    error: function(data, status) {
+        console.error(data.responseText);
+    }
+  });
+}
 
 function login() {
   window.location.replace('https://www.dropbox.com/1/oauth2/authorize?client_id=ojtyzbpz7peoc2v&response_type=code&redirect_uri=http://localhost:8000');
@@ -8,7 +24,7 @@ function login() {
 function auth() {
   var auth_code = getUrlParameter('code');
   if (!auth_code) return;
-  $.ajax({
+  var request = $.ajax({
       type: 'POST',
       url: 'https://api.dropbox.com/1/oauth2/token/',
       data: {
@@ -19,13 +35,15 @@ function auth() {
         client_secret: app_secret
       },
       success: function(data, status) {
+          var json = JSON.parse(data);
+          access_token = json.access_token;
+          console.log(access_token);
           console.log(data, status);
       },
       error: function(data, status) {
           console.error(data.responseText);
       }
     });
-
 }
 
 function getUrlParameter(sParam) {
@@ -42,3 +60,12 @@ function getUrlParameter(sParam) {
         }
     }
 };
+
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (access_token) {    
+          xhr.setRequestHeader ("Authorization", "Bearer " + access_token);
+        }
+    }
+});
