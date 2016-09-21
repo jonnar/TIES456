9482f3972db765161cdf5f30b20ae0b376c01dde
Matti Kinnunen <matti.kinnunen@jyu.fi>
2016-09-21 16:07:14 +0300

    Access token requests working. We are in business.

--- Task-2.2/main.js
+++ Task-2.2/main.js
@@ -1,13 +1,20 @@
 var app_key = 'ojtyzbpz7peoc2v';
 var app_secret = 'm8kdx1fyrghmj5d';
 
+function login() {
+  window.location.replace('https://www.dropbox.com/1/oauth2/authorize?client_id=ojtyzbpz7peoc2v&response_type=code&redirect_uri=http://localhost:8000');
+}
+
 function auth() {
+  var auth_code = getUrlParameter('code');
+  if (!auth_code) return;
   $.ajax({
       type: 'POST',
       url: 'https://api.dropbox.com/1/oauth2/token/',
       data: {
-        code: 'Q-UXgtawEKAAAAAAAAAAEABuHPT16v7r1_oQCvljvxE',
+        code: auth_code,
         grant_type: 'authorization_code',
+        redirect_uri: 'http://localhost:8000',
         client_id: app_key,
         client_secret: app_secret
       },
@@ -20,3 +27,18 @@ function auth() {
     });
 
 }
+
+function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
