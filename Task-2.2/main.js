7655766c8fa8782c73b54997b7331a165d459698
Matti Kinnunen <matti.kinnunen@jyu.fi>
2016-09-21 15:51:36 +0300

    Added query for ajax and first working auth request made to Dropbox. Does not work out of the box.

--- Task-2.2/main.js
+++ Task-2.2/main.js
@@ -1,3 +1,22 @@
-function clicked() {
-  alert("click");
+var app_key = 'ojtyzbpz7peoc2v';
+var app_secret = 'm8kdx1fyrghmj5d';
+
+function auth() {
+  $.ajax({
+      type: 'POST',
+      url: 'https://api.dropbox.com/1/oauth2/token/',
+      data: {
+        code: 'Q-UXgtawEKAAAAAAAAAAEABuHPT16v7r1_oQCvljvxE',
+        grant_type: 'authorization_code',
+        client_id: app_key,
+        client_secret: app_secret
+      },
+      success: function(data, status) {
+          console.log(data, status);
+      },
+      error: function(data, status) {
+          console.error(data.responseText);
+      }
+    });
+
 }
